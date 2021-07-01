e5c3f4b191c1b485913e24f5ee341abb92b2d4b4
andrew030987 <andrew.sev87@gmail.com>
2021-07-01 21:19:17 +0300

    to_do app models were created

--- to_do/models.py
+++ to_do/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Board(models.Model):
+    name = models.CharField(verbose_name='Name', max_length=100)
+
+    def __str__(self):
+        return f'Board {self.name}'
+
+
+class ToDoList(models.Model):
+    title = models.CharField(verbose_name='Title', max_length=100)
+    done = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_created=True)
+    update = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User, on_delete=models.PROTECT)
+    board = models.ForeignKey(Board, verbose_name='Board', on_delete=models.PROTECT)
+
+    def __str__(self):
+        return f'TodoList {self.title}, created at {self.created}'
+

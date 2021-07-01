c9d4b7cf13a15884c32ae29389479bfa2685a298
andrew030987 <andrew.sev87@gmail.com>
2021-07-01 21:53:03 +0300

    added REST API Board model

--- to_do/views.py
+++ to_do/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from to_do.serializers import BoardListAPIViewSerializer, BoardCreateAPIViewSerializer
+from to_do.models import Board
+from django.db.models import Count
 
-# Create your views here.
+
+class BoardListAPIView(generics.ListAPIView):
+    """
+    API to get all Boards with counts from To_do_list for each board
+    permission: - Is Authenticated
+    """
+    serializer_class = BoardListAPIViewSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def get_queryset(self):
+        queryset = Board.objects.annotate(count=Count('todolist__board'))
+        return queryset
+
+
+class BoardCreateAPIView(generics.CreateAPIView):
+    """
+    API to create new Board
+    permission: - Admin
+    """
+    serializer_class = BoardCreateAPIViewSerializer
+    permission_classes = [permissions.IsAdminUser]
+
+
+class BoardUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
+    """
+    API to update/delete Board
+    use row Id to execute
+    permission: - Admin
+    """
+    serializer_class = BoardCreateAPIViewSerializer
+    permission_classes = [permissions.IsAdminUser]
+    queryset = Board.objects.all()
+
+

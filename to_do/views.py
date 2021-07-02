4b52479344404ce3ff30b56932527aa774e572a2
andrew030987 <andrew.sev87@gmail.com>
2021-07-02 12:58:43 +0300

    added API: todo_create, todo_delete, todo_list, todo_list_undone, todo_update

--- to_do/views.py
+++ to_do/views.py
@@ -1,6 +1,6 @@
 from rest_framework import generics, permissions
-from to_do.serializers import BoardListAPIViewSerializer, BoardCreateAPIViewSerializer
-from to_do.models import Board
+from to_do.serializers import BoardListAPIViewSerializer, BoardCreateAPIViewSerializer, TodoListAPIViewSerializer
+from to_do.models import Board, ToDoList
 from django.db.models import Count
 
 
@@ -37,3 +37,69 @@ class BoardUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Board.objects.all()
 
 
+class TodoCreateApiView(generics.CreateAPIView):
+    """
+    API to create a new task for the board
+    permission: - Admin
+    """
+    serializer_class = TodoListAPIViewSerializer
+    permission_classes = [permissions.IsAdminUser, ]
+    queryset = ToDoList.objects.all()
+
+
+class TodoListApiView(generics.ListAPIView):
+    """
+    API to get list of all todos for boards
+    to perform: - use board row id
+    permissions: for all Authenticated
+    """
+    serializer_class = TodoListAPIViewSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, ]
+
+    def get_queryset(self):
+        queryset = ToDoList.objects.filter(board=self.kwargs['board'])
+
+
+class TodoListUnDoneApiView(generics.ListAPIView):
+    """
+    API to get list of all undone todos for boards
+    to perform: - use board row Id
+    permission: - for all authenticated
+    """
+    serializer_class = TodoListAPIViewSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, ]
+
+    def get_queryset(self):
+        queryset = ToDoList.objects.filter(board=self.kwargs['board']).filter(done=False)
+        return queryset
+
+
+class TodoDeleteApiView(generics.DestroyAPIView):
+    """
+    API to delete todos
+    use todolist row id
+    permission: - admin only
+    """
+    serializer_class = TodoListAPIViewSerializer
+    permission_classes = [permissions.IsAdminUser]
+    queryset = ToDoList.objects.all()
+
+
+class IsOwnerOrAdminPermission(permissions.BasePermission):
+    """
+    Check if object owner or staff
+    """
+
+    def has_object_permission(self, request, view, obj):
+        return obj.user == request.user or request.user.is_staff
+
+
+class TodoUpdateApiView(generics.RetrieveUpdateAPIView):
+    """"
+    API to update todos
+    use - todo_list row Id
+    permission: - object owner or admin
+    """
+    serializer_class = TodoListAPIViewSerializer
+    permission_classes = [IsOwnerOrAdminPermission, ]
+    queryset = ToDoList.objects.all()
